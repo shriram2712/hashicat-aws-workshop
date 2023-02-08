bbbe5a1dcc57d48e058aa25988779e3e4e83901a
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2023-02-08 11:02:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -207,7 +207,7 @@ resource "tls_private_key" "hashicat" {
 }
 
 locals {
-  private_key_filename = "${var.prefix}-ssh-key.pem"
+  private_key_filename = "${var.prefix}-ssh-key2.pem"
 }
 
 resource "aws_key_pair" "hashicat" {
